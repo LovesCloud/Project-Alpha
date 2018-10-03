967225e32e0e3fb8ffc08e8c9eacb19c602eb781
123rashmi <ruso.mishra@gmail.com>
2018-10-03 16:06:34 +0530

    thumbnail added

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="row">
             <div className="col-md-4">
               <div className="card mb-4 box-shadow">
-                <img className="card-img-top" src="thumbnail.svg" alt="Card image cap"/>
+                <img className="card-img-top" src={thumbnail} alt="Card image cap"/>
                 <div className="card-body">
                   <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <div className="d-flex justify-content-between align-items-center">
@@ -30,7 +30,7 @@ class App extends Component {
            
             <div className="col-md-4">
               <div className="card mb-4 box-shadow">
-                <img className="card-img-top" src="thumbnail.svg" alt="Card image cap"/>
+                <img className="card-img-top" src={thumbnail} alt="Card image cap"/>
                 <div className="card-body">
                   <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <div className="d-flex justify-content-between align-items-center">
@@ -42,10 +42,11 @@ class App extends Component {
                   </div>
                 </div>
               </div>
+			  </div>
             
             <div className="col-md-4">
               <div classNames="card mb-4 box-shadow">
-               <img className="card-img-top" src="thumbnail.svg" alt="Card image cap"/>
+              <img className="card-img-top" src={thumbnail} alt="Card image cap"/>
                 <div className="card-body">
                   <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <div className="d-flex justify-content-between align-items-center">
@@ -59,8 +60,9 @@ class App extends Component {
               </div>
              
 			</div>
+			
 			</div>
-      </div>
+      
      </div>
     );
   }
